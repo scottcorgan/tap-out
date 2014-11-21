bc7a6475a979cdfe781d52eb8f804ab321ab41ff
Scott Corgan <scottcorgan@gmail.com>
2014-11-21 10:03:46 -0800

    feature: add bin command "tap-out"

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
 var tapOut = require('../');
+
+var parser = tapOut(function (err, output) {
+  
+  process.stdout.write(JSON.stringify(output, null, 2));
+});
+
+process.stdin.pipe(parser);

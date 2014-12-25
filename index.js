4e75ad27d0acf9d1734839a694e46a2f4598edb9
Raynos <raynos2@gmail.com>
2014-12-25 16:14:58 -0500

    expose the parser as a sync interface

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+'use strict';
+
 var through = require('through2');
 var split = require('split');
 var trim = require('trim');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var reemit = require('re-emitter');
 
 var expr = require('./lib/utils/regexes');
 var parseLine = require('./lib/parse-line');
 
-module.exports = function (done) {
-  
-  done = done || function () {};
-  
-  var stream = through();
-  var testNumber = 0;
-  var writingErrorOutput = false;
-  var tmpErrorOutput = '';
-  var results = {
+function Parser() {
+  if (!(this instanceof Parser)) {
+    return new Parser();
+  }
+
+  EventEmitter.call(this);
+
+  this.results = {
     tests: [],
     asserts: [],
     versions: [],
@@ -22,6 +26,118 @@ module.exports = function (done) {
     pass: [],
     fail: [],
   };
+  this.testNumber = 0;
+  this.writingErrorOutput = false;
+  this.tmpErrorOutput = '';
+}
+
+util.inherits(Parser, EventEmitter);
+
+Parser.prototype.handleLine = function handleLine(line) {
+  var parsed = parseLine(line);
+
+  // This will handle all the error stuff
+  this._handleError(line);
+  
+  // THis is weird, but it's the only way to distinguish a
+  // console.log type output from an error output
+  if (!this.writingErrorOutput && !parsed && !isErrorOutputEnd(line)) {
+    var comment = {
+      type: 'comment',
+      raw: line,
+      test: this.testNumber
+    };
+    this.emit('comment', comment);
+    this.results.comments.push(comment);
+  }
+  
+  // Invalid line
+  if (!parsed) {
+    return;
+  }
+  
+  // Handle tests
+  if (parsed.type === 'test') {
+    this.testNumber += 1;
+    parsed.number = this.testNumber;
+  }
+  
+  // Handle asserts
+  if (parsed.type === 'assert') {
+    parsed.test = this.testNumber;
+    this.results[parsed.ok ? 'pass' : 'fail'].push(parsed);
+    
+    if (parsed.ok) {
+      // No need to have the error object
+      // in a passing assertion
+      delete parsed.error;
+      this.emit('pass', parsed);
+    }
+  }
+  
+  this.emit(parsed.type, parsed);
+  this.results[parsed.type + 's'].push(parsed);
+};
+
+Parser.prototype._handleError = function _handleError(line) {
+  // Start of error output
+  if (isErrorOutputStart(line)) {
+    this.writingErrorOutput = true;
+  }
+  // End of error output
+  else if (isErrorOutputEnd(line)) {
+    this.writingErrorOutput = false;
+    // Emit error here so it has the full error message with it
+    var lastAssert = this.results.fail[this.results.fail.length - 1];
+    this.emit('fail', lastAssert);
+  }
+  // No the beginning of the error message but it's the body
+  else if (this.writingErrorOutput) {
+    var lastAssert = this.results.fail[this.results.fail.length - 1];
+    var m = trim(line).split(': ');
+    
+    var msg = trim(m[1].replace(/['"]+/g, ''));
+    
+    if (m[0] === 'at') {
+      msg = trim(m[1]
+        .replace(/['"]+/g, '')
+        .replace('Test.<anonymous> (', ''))
+        .replace(')', '');
+      
+      var values = msg.split(':');
+      
+      msg = {
+        file: values[0],
+        line: values[1],
+        charactor: values[2]
+      };
+    }
+    
+    // This is a plan failure
+    if (lastAssert.name === 'plan != count') {
+      lastAssert.type = 'plan';
+      delete lastAssert.error.at;
+      lastAssert.error.operator = 'count';
+      
+      // Need to set this value
+      if (m[0] === 'actual') {
+        lastAssert.error.actual = trim(m[1]);
+      }
+    }
+    
+    lastAssert.error[m[0]] = msg;
+  }
+};
+
+module.exports = function (done) {
+  
+  done = done || function () {};
+  
+  var stream = through();
+  var parser = Parser();
+  reemit(parser, stream, [
+    'test', 'assert', 'version', 'result', 'pass', 'fail', 'comment'
+  ]);
   
   stream
     .pipe(split())
@@ -32,111 +148,17 @@ module.exports = function (done) {
       }
       
       var line = data.toString();
-      var parsed = parseLine(line);
-      
-      // This will handle all the error stuff
-      handleError(line);
-      
-      // THis is weird, but it's the only way to distinguish a
-      // console.log type output from an error output
-      if (!writingErrorOutput && !parsed && !isErrorOutputEnd(line)) {
-        var comment = {
-          type: 'comment',
-          raw: line,
-          test: testNumber
-        };
-        stream.emit('comment', comment);
-        results.comments.push(comment);
-      }
-      
-      // Invalid line
-      if (!parsed) {
-        return;
-      }
-      
-      // Handle tests
-      if (parsed.type === 'test') {
-        testNumber += 1;
-        parsed.number = testNumber;
-      }
-      
-      // Handle asserts
-      if (parsed.type === 'assert') {
-        parsed.test = testNumber;
-        results[parsed.ok ? 'pass' : 'fail'].push(parsed);
-        
-        if (parsed.ok) {
-          // No need to have the error object
-          // in a passing assertion
-          delete parsed.error;
-          stream.emit('pass', parsed);
-        }
-      }
-      
-      stream.emit(parsed.type, parsed);
-      results[parsed.type + 's'].push(parsed);
+      parser.handleLine(line);
     })
     .on('close', function () {
-      
-      stream.emit('output', results);
-      done(null, results);
+      stream.emit('output', parser.results);
+      done(null, parser.results);
     })
     .on('error', done);
   
-  //
-  function handleError (line) {
-    
-    // Start of error output
-    if (isErrorOutputStart(line)) {
-      writingErrorOutput = true;
-    }
-    // End of error output
-    else if (isErrorOutputEnd(line)) {
-      writingErrorOutput = false;
-      // Emit error here so it has the full error message with it
-      var lastAssert = results.fail[results.fail.length - 1];
-      stream.emit('fail', lastAssert);
-    }
-    // No the beginning of the error message but it's the body
-    else if (writingErrorOutput) {
-      var lastAssert = results.fail[results.fail.length - 1];
-      var m = trim(line).split(': ');
-      
-      var msg = trim(m[1].replace(/['"]+/g, ''));
-      
-      if (m[0] === 'at') {
-        msg = trim(m[1]
-          .replace(/['"]+/g, '')
-          .replace('Test.<anonymous> (', ''))
-          .replace(')', '');
-        
-        var values = msg.split(':');
-        
-        msg = {
-          file: values[0],
-          line: values[1],
-          charactor: values[2]
-        }
-      }
-      
-      // This is a plan failure
-      if (lastAssert.name === 'plan != count') {
-        lastAssert.type = 'plan';
-        delete lastAssert.error.at;
-        lastAssert.error.operator = 'count';
-        
-        // Need to set this value
-        if (m[0] === 'actual') {
-          lastAssert.error.actual = trim(m[1]);
-        }
-      }      
-      
-      lastAssert.error[m[0]] = msg;
-    }
-  }
-  
   return stream;
 };
+module.exports.Parser = Parser;
 
 function isErrorOutputStart (line) {
   

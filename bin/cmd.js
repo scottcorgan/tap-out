8545d142165dc8a4572b80a7e3246e27180e6d26
Scott Corgan <scottcorgan@gmail.com>
2015-12-03 17:10:50 -0800

    Prep for spike tests

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,21 +1,7 @@
 #!/usr/bin/env node
 
-var tapOut = require('../');
+var tapOut = require('../')
 
-var parser = tapOut(function (err, output) {
-  
-  if (err) {
-    throw err;
-  }
-  
-  var out = output;
-  
-  try {
-    out = JSON.stringify(output, null, 2);
-  }
-  catch (e) {}
-  
-  process.stdout.write(out);
-});
-
-process.stdin.pipe(parser);
+process.stdin
+  .pipe(tapOut.stream())
+  .pipe(process.stdout)
